644f2667487e0a937b3a6b8e95265a432f01d9a8
Sir Benjy <starvinbergvelvel@gmail.com>
2026-04-15 15:21:49 +0300

    Lobby is working properly (missing multiple entries' detection)

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import LoginPage from './pages/LoginPage.jsx'
 import SignupPage from './pages/SignupPage.jsx'
 import MenuWrapper from './MenuWrapper.jsx'
 import GameWrapper from './GameWrapper.jsx'
-import LobbyPage from "./pages/LobbyPage.jsx";
+import LobbyPage from "./pages/LobbyWrapper.jsx";
 
 // TEST
 import TeacherTestPage from './TEST/TeacherTestPage.jsx'
@@ -11,6 +11,7 @@ import StudentTestPage from './TEST/StudentTestPage.jsx'
 
 import './App.css'
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import LobbyWrapper from "./pages/LobbyWrapper.jsx";
 
 function App() {
     return (
@@ -21,7 +22,7 @@ function App() {
                 <Route path={"/signup"} element={<SignupPage />}/>
                 <Route path={"/menu"} element={<MenuWrapper />}/>
                 <Route path={"/game"} element={<GameWrapper />}/>
-                <Route path={"/lobby/:raceId"} element={<LobbyPage />}/>
+                <Route path={"/lobby/:raceId"} element={<LobbyWrapper />}/>
 
                 {/*TEST*/}
                 <Route path={"/test-tch"} element={<TeacherTestPage />}/>
--- src/pages/LobbyWrapper.jsx
+++ src/pages/LobbyWrapper.jsx
@@ -2,12 +2,14 @@ import { useEffect, useState } from "react";
 import { useNavigate, useParams } from "react-router-dom";
 import Cookies from "js-cookie";
 import axios from "axios";
-import { HOST } from "./Constants.js";
-import Lobby from "./pages/Lobby.jsx"; // Adjust the path if your Lobby is in a different folder
+import { HOST } from "../Constants.ts";
+import LobbyPage from "../pages/LobbyPage.jsx";
 
 function LobbyWrapper() {
     const navigate = useNavigate();
     const { raceId } = useParams();
+
+    // FIX 1: Removed the {} around null. Removed the unused 'role' state.
     const [user, setUser] = useState(null);
     const [loading, setLoading] = useState(true);
 
@@ -18,12 +20,22 @@ function LobbyWrapper() {
             return;
         }
 
-        // Fetch the user's details using their token
-        // (Replace "get-user" with whatever endpoint you currently use in your MenuWrapper)
-        axios.get(`${HOST}get-user`, { params: { token } })
+        axios.get(`${HOST}get-default-params`, { params: { token } })
             .then(res => {
                 if (res.data.success) {
-                    setUser(res.data.user);
+
+                    // FIX 2 & 3: Sync the Java data into the format the Lobby expects
+                    // Assuming userType 1 is Teacher and 2 is Student.
+                    // (Change these numbers if your database uses different ones!)
+                    const mappedRole = res.data.userType === 2 ? "teacher" : "student";
+
+                    // Rebuild the user object manually from the flat Java response
+                    setUser({
+                        id: res.data.userId,
+                        fullName: res.data.fullName,
+                        role: mappedRole
+                    });
+
                 } else {
                     navigate("/");
                 }
@@ -41,10 +53,9 @@ function LobbyWrapper() {
         return <div style={{ textAlign: 'center', marginTop: '50px' }}>Loading Lobby...</div>;
     }
 
-    // Here is where we fulfill your function Lobby({ user, role }) signature!
-    // We pass the user object, and extract the role (assuming your user object has a role property).
-    // If your backend doesn't send a 'role' property, you might need to determine it based on the user object.
-    return <Lobby user={user} role={user.role} />;
+    // Now 'user' has the exact structure we need, and 'user.role' will be a clean string!
+    console.log(user)
+    return <LobbyPage user={user} role={user.role} />;
 }
 
 export default LobbyWrapper;
