fb0dbf0911fb4c833efdef66733123bb3ecb1c3b
Sir Benjy <starvinbergvelvel@gmail.com>
2026-04-17 11:43:49 +0300

    Qfix

--- src/GameWrapper.jsx
+++ src/GameWrapper.jsx
@@ -1,6 +1,6 @@
 // if teacher -> TeacherGamePage else -> StudentGamePage
 import {useEffect, useState} from "react";
-import {useNavigate} from "react-router-dom";
+import {useNavigate, useParams} from "react-router-dom";
 import Cookies from "js-cookie";
 import axios from "axios";
 import {HOST} from "./Constants.js";
@@ -14,29 +14,50 @@ const USER_TYPES={
 }
 
 function GameWrapper(){
-    const [userType, setUserType] = useState(USER_TYPES.NONE)
     const navigate = useNavigate()
+    const [user, setUser] = useState(null);
+
+
+    const raceId = useParams();
 
     useEffect(() => {
         const token = Cookies.get("token")
         if(!token){
             navigate("/")
         }else{
-            axios.get(HOST + "get-user-type", {params:{token}})
-                .then((response) =>{
-                    setUserType(response.data.userType)
+            axios.get(HOST + "get-default-params", {params:{token, raceId}})
+                .then((res) =>{
+                    if (res.data.success) {
+
+                        // FIX 2 & 3: Sync the Java data into the format the Lobby expects
+                        // Assuming userType 1 is Teacher and 2 is Student.
+                        // (Change these numbers if your database uses different ones!)
+                        
+
+                        // Rebuild the user object manually from the flat Java response
+                        setUser({
+                            id: res.data.userId,
+                            fullName: res.data.fullName,
+                            role: res.data.userType
+                        });
+
+                    } else {
+                        alert("Failed to authenticate user "+ res.data.errorCode)
+                        navigate("/");
+                    }
                 })
         }
-    },[navigate, userType]);
-
-    if(userType === USER_TYPES.STUDENT){
-        return (
-            <StudentGamePage/>
-        )
-    }else if(userType===USER_TYPES.TEACHER){
-        return(
-            <TeacherGamePage/>
-        )
+    },[navigate, raceId]);
+    if(user){
+        if(user.role === USER_TYPES.STUDENT){
+            return (
+                <StudentGamePage/>
+            )
+        }else if(user.role===USER_TYPES.TEACHER){
+            return(
+                <TeacherGamePage/>
+            )
+        }
     }else{
         return(
             <div>
